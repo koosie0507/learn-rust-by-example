fc96c1e3696ff4332069e16546004a91d30abc67
Andrei Olar <andrei.olar@samlex.ro>
2019-12-16 18:47:36 +0200

    Add rust primitives
    -
    -
    -

---
+++ rust-primitives/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let logical: bool = true;
+    
+    let a_float: f64 = 1.0;
+    let an_integer = 5i32;
+
+    let default_float = 3.0;
+    let default_integer = 6;
+
+    let mut inferred_type = 12;
+    inferred_type = 429291929191i64;
+
+    let mut mutable = 12;
+    mutable = 21;
+
+    let mutable = true;
+}
